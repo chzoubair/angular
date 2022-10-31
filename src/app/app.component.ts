4478a1aa22211ec6ca833d0999f53fcacca906e9
chzoubair <chzoubair9@gmail.com>
2022-10-31 21:30:54 +0100

    project added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WeatherData } from './models/weather.model';
+import { WeatherService } from './services/weather.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'weather-application';
+
+export class AppComponent implements OnInit {
+  title(title: any) {
+    throw new Error('Method not implemented.');
+  }
+
+
+ constructor(private WeatherService: WeatherService) {
+
+ }
+ cityname: string ='paris';
+ weatherData?: WeatherData;
+
+  ngOnInit(): void {
+    this.getweatherdata(this.cityname);
+    
+    
+  } 
+  onSubmit() {
+    this.getweatherdata(this.cityname);
+    this.cityname='';
+
+  }
+  private getweatherdata(cityName: string){
+    this.WeatherService.getWeatherData('cityName')
+    .subscribe({
+      next: (Response)=> {
+        this.weatherData =Response;
+        console.log(Response);
+      }
+    });
+  }
+
+  
+  
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,13 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  weatherApiBaseUrl:'https://open-weather13.p.rapidapi.com/city/landon',
+  XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+  XRapidAPIHostHeaderValue:'open-weather13.p.rapidapi.com' ,
+  XRapidAPIKeyHeaderName:'X-RapidAPI-Key',
+  XRapidAPIKeyHeaderValue: 'd8bd11eda3msh4eb5b619017b378p157e67jsnb31f5d1cd41e',
+
 };
 
 /*
